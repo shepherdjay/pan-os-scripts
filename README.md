7c9720603028529d3d15ede411027fdb4c432d38
Dharma Bellamkonda <Dharma.Bellamkonda@maverik.com>
2015-10-16 15:27:24 -0600

    Load credentials from file

---
+++ README.md
@@ -0,0 +1,14 @@
+## Panorama Firewall Scripts
+
+### Quick Start
+
+Install depdendencies:
+
+```
+pip install -r requirements.txt
+```
+
+Copy `config.yml.example` to `config.yml` and configure the values:
+
+- `panorama_hostname`: Hostname of your Panorama firewall
+- `panorama_api_key`: Valid API key for your firewall
--- pan-export.py
+++ pan-export.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 __author__ = 'js201393'
 
-import xlwt
 import pan.xapi
+import yaml
 
-panorama_api_key =
-dc02_fw01_api_key =
+
+class Config:
+    def __init__(self, filename):
+        with open(filename, 'r') as stream:
+            config = yaml.load(stream)
+        self.panorama_hostname = config["panorama_hostname"]
+        self.panorama_api_key = config["panorama_api_key"]
 
 
 def gather_rules(hostname, api_key):
     cmd_path = '<show><config><pushed-shared-policy></pushed-shared-policy></config></show>'
-    firewall = pan.xapi.PanXapi(hostname=hostname,
-                                api_key=api_key
-                                )
+    firewall = pan.xapi.PanXapi(hostname=hostname, api_key=api_key)
     firewall.op(cmd=cmd_path)
     return firewall.xml_result()
 
 
-dc02_fw01_rules = gather_rules('dc02-fw01.maverik.com', dc02_fw01_api_key)
-print(dc02_fw01_rules)
+def main():
+    config = Config("config.yml")
+    print(gather_rules(config.panorama_hostname, config.panorama_api_key))
+
+
+if __name__ == "__main__":
+    main()
