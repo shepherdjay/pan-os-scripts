8f870f88b7acc51c81ba9ed410b2ec68ad99233e
Jay Shepherd <Jay.Shepherd@maverik.com>
2016-01-07 15:54:52 -0700

    Refined modularity for testing

--- pancompare_tests.py
+++ pancompare_tests.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
+
 import pancompare
 
 
 class PancompareTests(TestCase):
     def test_standard_hex(self):
-        self.assertEqual(pancompare.hex_to_ipv6('2607f8b0400a0806000000000000200a'), '2607:f8b0:400a:0806:0000:0000:0000:200a')
+        self.assertEqual(pancompare.hex_to_ipv6('2607f8b0400a0806000000000000200a'),
+                         '2607:f8b0:400a:0806:0000:0000:0000:200a')
 
+    def test_rule_match(self):
+        script_config = pancompare.Config('testfiles/filters_test.yml')
+        with open('testfiles/test_rule.txt', 'r') as file:
+            test_rule = file.read()
+        filters = script_config.rule_filters
+        self.assertTrue(type(pancompare.filter_dataplane_rules(test_rule, filters) is None))
