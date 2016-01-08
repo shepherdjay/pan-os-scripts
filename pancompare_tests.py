225c03fc4ee98d70e8a416f8174934df0a4a0513
Jay Shepherd <Jay.Shepherd@maverik.com>
2016-01-07 17:13:21 -0700

    Uploading sanatized test files

--- pancompare_tests.py
+++ pancompare_tests.py
@@ -8,9 +8,14 @@ class PancompareTests(TestCase):
         self.assertEqual(pancompare.hex_to_ipv6('2607f8b0400a0806000000000000200a'),
                          '2607:f8b0:400a:0806:0000:0000:0000:200a')
 
-    def test_rule_match(self):
+    def test_dataplane_no_match(self):
+        """
+        This test itterates through the dataplane no match file which should contain no rules that match the
+        included filters test.
+        :return:
+        """
         script_config = pancompare.Config('testfiles/filters_test.yml')
-        with open('testfiles/test_rule.txt', 'r') as file:
+        with open('testfiles/test_raw_dataplane_nomatch.txt', 'r') as file:
             test_rule = file.read()
         filters = script_config.rule_filters
         self.assertTrue(type(pancompare.filter_dataplane_rules(test_rule, filters) is None))
