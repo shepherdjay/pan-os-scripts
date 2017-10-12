2cded57e004e788c7992c679e19497b674161746
Jay Shepherd <jay.shepherd@maverik.com>
2017-10-12 13:14:46 -0600

    merge dictionaries function initial write and tests

--- panobjectfinder.py
+++ panobjectfinder.py
@@ -22,11 +22,44 @@ def merge_dictionaries(dict1, dict2):
     Takes a list of dictionaries and merges them. If a key is conflicted it is instead added to an error list.
     :param dict1: First dictionary
     :param dict2: Second dictionary
-    :return: Merged dictionary plus erros
+    :return: Merged dictionary plus errors as unique list
     """
-    dict = {}
+    result_dict = {}
     errors = []
-    return dict, errors
+
+    for key, value in dict1.items():
+        try:
+            if value == dict2[key]:
+                key_match = True
+            else:
+                key_match = False
+        except (KeyError, TypeError):
+            key_match = None
+        if key_match is not None:
+            if key_match:
+                result_dict[key] = value
+            else:
+                errors.append(key)
+        else:
+            result_dict[key] = value
+
+    for key, value in dict2.items():
+        try:
+            if value == dict1[key]:
+                key_match = True
+            else:
+                key_match = False
+        except (KeyError, TypeError):
+            key_match = None
+        if key_match is not None:
+            if key_match:
+                result_dict[key] = value
+            else:
+                errors.append(key)
+        else:
+            result_dict[key] = value
+
+    return result_dict, list(set(errors))
 
 
 def find_objects(firewall_config, object_list):
--- test_panobjectfinder.py
+++ test_panobjectfinder.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 import xmltodict
+import panobjectfinder
 
 TEST_FILE_DIR = "testfiles/"
 
@@ -17,7 +18,30 @@ def get_test_path(file):
 
 class TestPanObjectFinder(TestCase):
     def test_merge_dictionaries(self):
-        return True
+        dict1 = {'a': 1}
+        dict2 = {'b': 2}
+        expected_dict = {'a': 1, 'b': 2}
+
+        resulted_dict, errors = panobjectfinder.merge_dictionaries(dict1, dict2)
+        self.assertDictEqual(expected_dict, resulted_dict)
+        self.assertEqual(errors, [])
 
     def test_merge_dictionaries_with_errors(self):
-        return True
+        dict1 = {'a': 1}
+        dict2 = {'a': 2}
+        expected_dict = {}
+        expected_errors = ['a']
+
+        resulted_dict, errors = panobjectfinder.merge_dictionaries(dict1, dict2)
+        self.assertDictEqual(expected_dict, resulted_dict)
+        self.assertEqual(errors, expected_errors)
+
+    def test_merge_dictionaries_different_sizes_and_errors(self):
+        dict1 = {'a': 1, 'b': 2}
+        dict2 = {'b': 3, 'c': 4, 'd': 5}
+        expected_dict = {'a': 1, 'c': 4, 'd': 5}
+        expected_errors = ['b']
+
+        resulted_dict, errors = panobjectfinder.merge_dictionaries(dict1, dict2)
+        self.assertDictEqual(expected_dict, resulted_dict)
+        self.assertEqual(errors, expected_errors)
